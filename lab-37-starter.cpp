86dc2ffc01257cbe2a0a7ea9744f673e1db3702a
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-12-01 17:38:37 -0800

    5:38 PM

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -3,24 +3,15 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <list>
 #include <fstream>
 
 using namespace std;
 
-int sum_ascii(const string &code);
+int gen_hash_index(const string &code);
 
 int main() {
-    cout<<endl;
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    string test="18B51F64622B";
-   cout<<sum_ascii(test)<<endl;
-   // produces 669 i pulled a random code from the text and used it as an example
-
     //file path : C:\\Users\\hope4\\Desktop\\COMSC 210 Work\\210-Lab-37\\lab-37-data.txt
     cout<<"Enter file path! ";
     string filepath;
@@ -35,26 +26,20 @@ int main() {
     }
     string line;
     int totalsum=0;
+    map<int,list<string>>hash_table;
     while(getline(file,line)){
-        totalsum+=sum_ascii(line);
+        //totalsum+=sum_ascii(line);
     }
     cout<<totalsum;
+
+   
     return 0;
 }
 
-int sum_ascii(const string &code){
+int gen_hash_index(const string &code){
     int sum=0;
     for(char index: code){
         sum+=(int)index;
     }
     return sum;
 }
-
-
-/* 
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
