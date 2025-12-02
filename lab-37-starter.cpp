af6a1234bcbe32ab7631f81259002a251bb8d65b
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-12-01 17:17:55 -0800

    5:17 PM

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -1,18 +1,34 @@
+// COMSC-210 | Lab 37 | Daniel Santisteban
+// IDE used: VS Code
+
 #include <iostream>
+#include <string>
 using namespace std;
 
+int sum_ascii();
 
 int main() {
+    cout<<endl;
     char a = 'A';
     cout << a << endl;
     cout << (int) a << endl;
     int b = 66;
     cout << b << endl;
     cout << (char) b << endl;
-    
-
+    string test="gurt";
+   cout<<sum_ascii(test);
     return 0;
 }
+
+int sum_ascii(const string &code){
+    int sum;
+    for(char index: code){
+        sum+=(int)index;
+    }
+
+    return sum;
+}
+
 
 /* 
 These targets are present in the dataset and can be used for testing:
